85144caf20ed2e16024ad35069706f69adbeebdb
Paul Croft <pcroft@desktop.isilon.com>
2015-08-21 15:10:32 -0700

    Some small improvments. Added command line argument for the number of names to create

--- namer.py
+++ namer.py
@@ -6,23 +6,30 @@
 import cPickle
 from pprint import pprint, pformat
 import random
-
+from sys import argv
 
 def main():
+    todo = 10
+    if len(argv) - 1:
+        try:
+            todo = int(argv[1])
+        except ValueError:
+            pass
+
     cdata = None
     with open("data.cpk") as infile:
         cdata = cPickle.loads(infile.read())
 
 #    pprint(cdata,depth=2)
 #    pprint(cdata["Special Machine Names"]["Name"])
-    for i in range(10):
-        if not random.randint(0,3):
+    for i in range(todo):
+        if not random.randint(0, 3):
             print random.choice(cdata["Special Machine Names"]["Name"])
         else:
-            Adj = random.choice(reduce(lambda x,y:x+y, cdata["Adjective List"].values()))
-            Noun = random.choice(reduce(lambda x,y:x+y, cdata["Noun List"].values()))
+            Adj = random.choice(reduce(lambda x, y: x+y, cdata["Adjective List"].values()))
+            Noun = random.choice(reduce(lambda x,y : x+y, cdata["Noun List"].values()))
 
-            print Adj,Noun
+            print Adj, Noun
 
 
 
